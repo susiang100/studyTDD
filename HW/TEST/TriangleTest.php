7df6164c3ce46b753733395e2c3b1bb91501a35d
susiang100 <susiang100@gmail.com>
2014-10-26 05:28:39 +0800

    HW1_TriangleTest_v1.2(26 Oct 2014) - 優化

--- HW/TEST/TriangleTest.php
+++ HW/TEST/TriangleTest.php
@@ -3,47 +3,71 @@
 require_once __DIR__ . '/../Triangle.php';
 
 class TriangleTest extends PHPUnit_Framework_TestCase {
-    public function setUp() {
+
+	private function ValidateFunc($thisSet="",$expected="",$e=""){
+		switch ($thisSet) {
+			case 1:
+			if ($expected == "ok") {//Detected(no error)
+				echo "no_msg(",$expected,")->OK", "\n";//達到預期效果，assert OK
+			} else {
+				echo "no_msg(",$expected,")->X!", "\n";//非預期效果，assert 有問題
+			}
+			$this -> assertFalse(false);
+			break;
+
+			case 2:
+			if ($expected == "error") {//Detected(have error)
+				echo "msg(",$e -> getMessage(),")->OK", "\n";//達到預期效果，assert OK
+			} else {
+				echo "msg(",$e -> getMessage(),")->X!", "\n";//非預期效果，assert 有問題
+			}
+			$this -> assertTrue(true);
+			break;
+        }
+    }
+
+	public function setUp() {
         $this -> triangle = new Triangle();
     }
+
 	public function myProviderInt(){
 		return array(
-			array("a", 1, 1, "error int"),
-			array(1, "a", 1, "error int"),
-			array(1, 1, "a", "error int"),
-			array("a", "a", 1, "error int"),
-			array(1, "a", "a", "error int"),
-			array("a", 1, "a", "error int"),
-			array("a", "a", "a", "error int"),
-			array(null, null, null, "error int"),
-			//array(0, 0, 0, "error int"),
-			array(0, 0, 0, "ok int"),
-			array(-100, 0, 100, "ok int"),
-			array(1, 1, 2147483647, "ok int"),//32-bit is True
-			array(1, 1, -2147483647, "ok int"),//32-bit is True
-			array(1, 1, 2147483648, "ok int"),//64-bit is True
-			array(1, 1, -2147483648, "ok int"),//64-bit is True
-			array(1, 1, 9223372036854775808, "ok int"),//All is False
-			array(1, 1, -9223372036854775808, "ok int"),//All is False
+			array("a", 1, 1, "error"),
+			array(1, "a", 1, "error"),
+			array(1, 1, "a", "error"),
+			array("a", "a", 1, "error"),
+			array(1, "a", "a", "error"),
+			array("a", 1, "a", "error"),
+			array("a", "a", "a", "error"),
+			array(NULL, NULL, NULL, "error"),
+			//array(0, 0, 0, "error"),
+			array(0, 0, 0, "ok"),
+			array(-100, 0, 100, "ok"),
+			array(1, 1, 2147483647, "ok"),//32-bit is True
+			array(1, 1, -2147483647, "ok"),//32-bit is True
+			array(1, 1, 2147483648, "ok"),//64-bit is True
+			array(1, 1, -2147483648, "ok"),//64-bit is True
+			array(1, 1, 9223372036854775808, "ok"),//All is False
+			array(1, 1, -9223372036854775808, "ok"),//All is False
         );
     }
 	
 		public function myProviderRange(){
 		return array(
-			array(0, 0, 0, "error range"),
-			array(0, 10, 10, "error range"),
-			array(10, 0, 10, "error range"),
-			array(10, 10, 0, "error range"),
-			array(-1, -1, -1, "error range"),
+			array(0, 0, 0, "error"),
+			array(0, 10, 10, "error"),
+			array(10, 0, 10, "error"),
+			array(10, 10, 0, "error"),
+			array(-1, -1, -1, "error"),
         );
     }
 	
 		public function myProviderFormation(){
 		return array(
-			array(100, 1, 1, "error formation"),
-			array(1, 100, 1, "error formation"),
-			array(1, 10, 1000, "error formation"),
-			array(3, 4, 5, "ok formation"),
+			array(100, 1, 1, "error"),
+			array(1, 100, 1, "error"),
+			array(1, 10, 1000, "error"),
+			array(3, 4, 5, "ok"),
 
         );
     }
@@ -52,7 +76,7 @@ class TriangleTest extends PHPUnit_Framework_TestCase {
 		return array(
 			array(3, 4, 5, 3),//不等邊三角形
 			array(9, 9, 1, 2),//等邊三角形
-			array(9, 9, 1, 1),//等邊三角形,但預設為1正三角形
+			array(9, 9, 1, 1),//等邊三角形,但預設結果為1(正三角形)，assertion錯誤！
 			array(9, 9, 9, 1),//正三角形
  
         );
@@ -61,46 +85,26 @@ class TriangleTest extends PHPUnit_Framework_TestCase {
     /**
      * @dataProvider myProviderInt
      */
-    public function testValidateInt($a='a',$b=1,$c=1,$expected='error int'){
-		echo __FUNCTION__,"(",$a,",",$b,",",$c,",",$expected,") --> " ;
-        try { 
+    public function testValidateInt($a='a',$b=1,$c=1,$expected='error'){
+		echo __FUNCTION__,"(",$a,",",$b,",",$c,",",$expected,") --> ";
+		try {
 			$this -> triangle -> validateInt($a,$b,$c);
-			if ($expected == "ok int") {//no error
-				echo "msg: ok int/assert_1 OK", "\n";//達到預期效果，assert OK
-			} else {
-				echo "assert_1 X!", "\n";//非預期效果，assert 有問題
-			}
-			$this -> assertFalse(false);
-        } catch(Exception $e) {//have error
-			if ($expected == "error int") {//達到預期效果，assert OK
-				echo 'msg: ',  $e -> getMessage(), "/";echo "assert_1 OK", "\n";
-			} else {//非預期效果，assert 有問題
-				echo "assert_1 X!", "\n";
-			}
-			$this -> assertTrue(true);
+			$this -> ValidateFunc(1,$expected);
+        } catch(Exception $e) {
+			$this -> ValidateFunc(2,$expected,$e);
         }
     }
-	
+
     /**
      * @dataProvider myProviderRange
      */
-    public function testValidateRange($a='0',$b=0,$c=0,$expected='error range') {
-		echo __FUNCTION__,"(",$a,",",$b,",",$c,",",$expected,") --> " ;
-        try {
-			$this -> triangle -> validateRange($a, $b, $c);
-			if ($expected == "ok range") {
-				echo "msg: ok range/assert_2 OK", "\n";
-			} else {
-				echo "assert_2 X!", "\n";
-			}
-			$this -> assertFalse(false);
+    public function testValidateRange($a=0,$b=0,$c=0,$expected='error') {
+		echo __FUNCTION__,"(",$a,",",$b,",",$c,",",$expected,") --> ";
+		try {
+			$this -> triangle -> validateRange($a,$b,$c);
+			$this -> ValidateFunc(1,$expected);
         } catch(Exception $e) {
-			if ($expected == "error range") {
-				echo 'msg: ',  $e -> getMessage(), "/";echo "assert_2 OK", "\n";
-			} else {
-				echo "assert_2 X!", "\n";
-			}
-            $this -> assertTrue(true);
+			$this -> ValidateFunc(2,$expected,$e);
         }
     }
 
@@ -108,40 +112,29 @@ class TriangleTest extends PHPUnit_Framework_TestCase {
      * @dataProvider myProviderFormation
      */
     public function testValidateFormation($a=1,$b=0,$c=0,$expected='error formation') {
-		echo __FUNCTION__,"(",$a,",",$b,",",$c,",",$expected,") --> " ;
-        try {
-			$this -> triangle -> validateFormation($a, $b, $c);
-			if ($expected == "ok formation") {
-				echo "msg: ok formation/assert_3 OK", "\n";
-			} else {
-				echo "assert_3 X!", "\n";
-			}
-			$this -> assertFalse(false);
+		echo __FUNCTION__,"(",$a,",",$b,",",$c,",",$expected,") --> ";
+		try {
+			$this -> triangle -> validateFormation($a,$b,$c);
+			$this -> ValidateFunc(1,$expected);
         } catch(Exception $e) {
-			if ($expected == "error formation") {
-				echo 'msg: ',  $e -> getMessage(), "/";echo "assert_3 OK", "\n";
-			} else {
-				echo "assert_3 X!", "\n";
-			}
-            $this -> assertTrue(true);
+			$this -> ValidateFunc(2,$expected,$e);
         }
     }
 	
     /**
      * @dataProvider myProviderTypes
      */
-    public function testValidateTypes($a=3,$b=4,$c=5,$expected=3) {
-		echo __FUNCTION__,"(",$a,",",$b,",",$c,",",$expected,") --> " ;
-        //$this -> assertEquals($expected, $this -> triangle -> validateTypes($a, $b, $c));
-		//echo "this Types OK\n";
-       try {
-			$this -> assertEquals($expected, $this -> triangle -> validateTypes($a, $b, $c));
-			echo "msg: ok types/assert_4 OK1", "\n";
+    public function testTriangle($a=3,$b=4,$c=5,$expected=3) {
+		echo __FUNCTION__,"(",$a,",",$b,",",$c,",",$expected,") --> ";
+		try {
+			$this -> assertEquals($expected, $this -> triangle -> calTriangle($a, $b, $c));
+			echo "result(",$expected,")->OK", "\n";//達到預期效果，assert OK
 			//$this -> assertFalse(false);
-        } catch(Exception $e) {
-			echo 'msg: ',  $e -> getMessage(), "\n";
-            //$this -> assertTrue(true);
+		} catch(Exception $e) {
+			echo "result(",$this -> triangle -> calTriangle($a, $b, $c),")->X!", "\n";//非預期效果，assert 有問題
+			//echo 'msg: ',  $e -> getMessage(), "\n";
+			//$this -> assertTrue(true);
         }
-		
     }
 }
+?>
