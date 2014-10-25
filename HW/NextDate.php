86a567edeeee8d69b45fd4f521c8c9f433e3c09a
susiang100 <susiang100@gmail.com>
2014-10-26 03:03:16 +0800

    HW1_NextDate_v1.1(26 Oct 2014)

--- HW/NextDate.php
+++ HW/NextDate.php
@@ -6,7 +6,6 @@ class NextDate {
     public function validateInt($a, $b, $c) {
 		if(!is_int($a) || !is_int($b) || !is_int($c)) {
 			throw new Exception("error int");
-			//echo "error int";
         }
     }
 	
@@ -15,93 +14,68 @@ class NextDate {
 		$V2_a = 9999;
 		$V1_b = 1;
 		$V2_b = 12;
-        //if (($a < $V1_a || $a > $V2_a) && ($b < $V1_b || $b > $V2_b) && ($c < 1 || $c > 31)) {
-			//error year, month, day
-			//throw new Exception('error year, month, day');
-		//} else 
-		if (($a >= $V1_a && $a <= $V2_a) && ($b >= $V1_b && $b <= $V2_b)) {
-			//ok year, month
-			if ($c >= 1) {//ok year, month, day
-				if ($b==2) {
-					if (is_int($a/4) && $c > 29) {
-						throw new Exception('error day(Max:29)');
-					} else if (!is_int($a/4) && $c > 28) {
+		if ( ($a>=$V1_a) && ($a<=$V2_a) ) {//ok year
+			if ($c>=1) {//ok year >> ok day(Min:1)
+				if ( ($b<$V1_b) || ($b>$V2_b) ) {//ok year >> ok day(Min:1) >> error month(Min:1,Max:12)
+					throw new Exception('error month');
+				} else {//ok year >> ok day(Min:1) >> ok month(Min:1,Max:12)
+					if ( ($b==2) && (!is_int($a/4)) && ($c>28) ) {
 						throw new Exception('error day(Max:28)');
-					}
-				} else if ($b==4 || $b==6 || $b==9 || $b==11) {
-					if ($c > 30) {
+					} else if ( ($b==2) && (is_int($a/4)) && ($c>29) ) {
+						throw new Exception('error day(Max:29)');
+					} else if ( ($b==4 || $b==6 || $b==9 || $b==11) && ($c>30) ) {
 						throw new Exception('error day(Max:30)');
-					}
-				} else { //if ($b==1 || $b==3 || $b==5 || $b==7 || $b==8 || $b==10 || $b==12) {
-					if ($c > 31) {
+					} else if ($c>31) {//if ($b==1 || $b==3 || $b==5 || $b==7 || $b==8 || $b==10 )
 						throw new Exception('error day(Max:31)');
 					}
 				}
-			} else if ($c <= 0) {//ok year, month, error day
-				throw new Exception('error day(Min:1)');
-			}
-		} else if (($a >= $V1_a && $a <= $V2_a)) {
-			//ok year
-			if ($c >= 1) {//ok year, day
-				if ($b <= 0) {//ok year, day, error month
-					throw new Exception('error month(Min:1)');
-				}
-			} else if ($c <= 0) {//ok year, error day
-				if ($b <= 0) {//ok year, error month, day
-					throw new Exception('error month(Min:1), day(Min:1)');
+			} else { //if ($c <= 0) {//ok year >> error day(Min:1)
+				if ( ($b<$V1_b) || ($b>$V2_b) ) {//ok year >> error day(Min:1) >> error month(Min:1,Max:12)
+					throw new Exception('error month, day');
+				}  else {
+					throw new Exception('error day');
 				}
 			}
-		} else if (($a < $V1_a || $a > $V2_a)) {
-			//error year
-			if ($c >= 1) {//error year, ok day
-				if ($b <= 0) {//error year, month, ok day
-					throw new Exception('error year, month(Min:1)');
-				} else { //error year, ok month, day
-					throw new Exception('error year');
+		} else if (($a < $V1_a || $a > $V2_a)) {//error year
+			if ($c>=1) {//error year >> ok day(Min:1)
+				if ( ($b<$V1_b) || ($b>$V2_b) ) {//error year >> ok day(Min:1) >> error month(Min:1,Max:12)
+					throw new Exception('error year, month');
+				} else {//error year >> ok day(Min:1) >> ok month(Min:1,Max:12)
+					if ( ($b==2) && ($c>28) ) {
+						throw new Exception('error year, error day(Max: 28/29)');
+					} else if ( ($b==4 || $b==6 || $b==9 || $b==11) && ($c>30) ) {
+						throw new Exception('error year, error day(Max:30)');
+					} else if ($c>31) {//if ($b==1 || $b==3 || $b==5 || $b==7 || $b==8 || $b==10 )
+						throw new Exception('error year, error day(Max:31)');
+					} else {
+						throw new Exception('error year');
+					}
 				}
-			} else if ($c <= 0) {//error year, day
-				if ($b <= 0) {//error year, month, day
-					throw new Exception('error year, month(Min:1), day(Min:1)');
-				} else { //error year, day, ok month
-					throw new Exception('error year, day(Min:1)');
+			} else { //if ($c <= 0) {//error year >> error day(Min:1)
+				if ( ($b<$V1_b) || ($b>$V2_b) ) {//error year >> error day(Min:1) >> error month(Min:1,Max:12)
+					throw new Exception('error year, month, day');
+				}  else {
+					throw new Exception('error year, day');
 				}
 			}
-
-		} else {
-			throw new Exception('unknow error');
 		}
 	}
 			
-
     public function calNextDate($a, $b, $c) {
-		if ($b == 12 && $c == 31) {
-			$a = $a+1;
+		if ($b == 12 && $c == 31) {//NextYear
+			$a++;
 			$b = 1;
 			$c = 1;
-		} else if ($b==2) {
-			if (is_int($a/4) && $c == 29) {
-				$b = $b+1;
-				$c = 1;
-			} else if (!is_int($a/4) && $c == 28) {
-				$b = $b+1;
-				$c = 1;
-			} else {
-				$c = $c+1;
-			}
-		} else if (($b==4 || $b==6 || $b==9 || $b==11)) {
-			if ($c == 30) {
-				$b = $b+1;
-				$c = 1;
-			} else {
-				$c = $c+1;
-			}
-		} else { //if ($b==1 || $b==3 || $b==5 || $b==7 || $b==8 || $b==10 || $b==12) {
-			if ($c == 31) {
-				$b = $b+1;
-				$c = 1;
-			} else {
-				$c = $c+1;
-			}
+		} else if (
+			( ($b==2) && (!is_int($a/4)) && ($c==28) ) ||
+			( ($b==2) && (is_int($a/4)) && ($c==29) ) ||
+			( ($b==4 || $b==6 || $b==9 || $b==11) && ($c == 30) ) ||
+			($c == 31)  //if ($b==1 || $b==3 || $b==5 || $b==7 || $b==8 || $b==10 || $b==12) 
+		) {//NextMonth
+			$b++;
+			$c = 1;
+		} else {//NextDay
+			$c++;
 		}
 		$result = (string)($a."-".$b."-".$c);
 		return $result;
