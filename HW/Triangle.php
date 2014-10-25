a4d5bc324f340abad0b85152d265c2c0d115e2e5
susiang100 <susiang100@gmail.com>
2014-10-26 05:29:04 +0800

    HW1_Triangle_v1.2(26 Oct 2014) - 優化

--- HW/Triangle.php
+++ HW/Triangle.php
@@ -1,30 +1,9 @@
 <?php
-class Triangle {
 /**  
 * Triangle
 */
-/*///Testing input var
-	public $a,$b,$c = '';
-    public function __construct($cfg=NULL) {
-        if ($cfg) {
-            $this->config($cfg);
-        }
-    }
-    private function config($cfg='') {
-        if (is_string($cfg)) {
-            $cfg = require $cfg;
-        }
-        if (isset($cfg['a'])) {
-            $this->a = $cfg['a'];
-        }
-        if (isset($cfg['b'])) {
-            $this->b = $cfg['b'];
-        }
-        if (isset($cfg['c'])) {
-            $this->c = $cfg['c'];
-        }
-    }
-*/
+class Triangle {
+	
     public function validateInt($a, $b, $c) {
 		if(!is_int($a) || !is_int($b) || !is_int($c)) {
 			throw new Exception("error int");
@@ -34,7 +13,6 @@ class Triangle {
     public function validateRange($a, $b, $c) {
         if($a < 1 || $b < 1 || $c < 1) {
             throw new Exception('error range');
-			return 1;
         }
     }
 
@@ -46,7 +24,7 @@ class Triangle {
         }
     }
 	
-    public function validateTypes($a, $b, $c) {
+    public function calTriangle($a, $b, $c) {
         if($a === $b && $b === $c && $c === $a) {
 			return 1;//正三角形
         } elseif($a === $b || $b === $c || $c === $a) {
@@ -56,6 +34,4 @@ class Triangle {
         }
     }
 }
-
-//$C_Triangle = new Triangle();
-//$C_Triangle -> validateInt("1",1,1);
+?>
